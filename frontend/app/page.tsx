c4f666d7cefd9925fa72512d3ac86b72aea5ef36
Anson Antony <ansonantony89@gmail.com>
2024-10-20 00:34:39 +0530

    main

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,25 @@
-// import Lamp from "@/components/ui/lamp";
+import Lamp from "@/components/ui/lamp";
 import SignupForm from "@/components/example/signup-form-demo";
+import DepositCard from "@/components/ui/DepositCard";
+import Link from "next/link";
 
 export default function Home() {
   return (
    <>
-      {/* <Lamp/> */}
+      <ul className="navbar-links">
+        <li>
+          <Link href="/">Deposit</Link>
+        </li>
+        <li>
+          <Link href="/swap">Swap</Link>
+        </li>
+        <li>
+          <Link href="/luckydraw">Lucky Draw</Link>
+        </li>
+      </ul>
+      <Lamp/>
       <SignupForm/>
+      <DepositCard/>
    </>
   );
 }
--- frontend/components/ui/lamp.tsx
+++ frontend/components/ui/lamp.tsx
@@ -1,11 +1,18 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
+import   Navbar   from "./navbar-menu";
 
 export default function Lamp() {
+  const [active, setActive] = useState<string | null>(null);
   return (
-    <LampContainer>
+  <>
+  <Navbar/>
+    {/* <LampContainer> */}
+      
+      
+      
       <motion.h1
         initial={{ opacity: 0.5, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -16,9 +23,10 @@ export default function Lamp() {
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        Build lamps <br /> the right way
+        DeFin <br /> the right way
       </motion.h1>
     </LampContainer>
+    </>
   );
 }
 
